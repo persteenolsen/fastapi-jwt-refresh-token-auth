e2f2489b22f41c8f7cd81cea6741f5d8f8f0d481
Per Olsen <persteen22@gmail.com>
2026-06-13 14:33:37 +0200

    Adjusted app.py

--- api/api.py
+++ api/api.py
@@ -20,7 +20,7 @@ models.user.Base.metadata.create_all(bind=engine)
 app = FastAPI(
 
     title="Python + FastApi + PostgreSQL + Alembic + Auth by JWT with Refresh Token Renewal",
-    description="12-06-2026 - FastAPI serving Authentication by JWT with Refresh Token Renewal using these credentials: testuser / admin",
+    description="13-06-2026 - FastAPI serving Authentication by JWT with Refresh Token Renewal using these credentials: testuser / admin",
     version="0.0.1",
 
     contact={
