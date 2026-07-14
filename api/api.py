ec873ec89fab4580c752f71391d89433c790ee06
Per Olsen <persteen22@gmail.com>
2026-07-14 13:08:52 +0200

    Updated the api meta data - title and description

--- api/api.py
+++ api/api.py
@@ -20,8 +20,8 @@ from routes.simple import router_simple as router_simple_one
 # Initialize the FastAPI app
 app = FastAPI(
 
-    title="Python + FastApi + PostgreSQL + Alembic + Auth by JWT with Refresh Token Renewal",
-    description="07-07-2026 - FastAPI serving Authentication by JWT with Refresh Token Renewal using these credentials: testuser / admin",
+    title="FastAPI Authentication Using JWT and Refresh Token Renewal",
+    description="14-07-2026 - FastAPI authentication service using JWT and Refresh Token Renewal with PostgreSQL for data persistence and Alembic for database migrations. Demo credentials: testuser / admin",
     version="0.0.1",
 
     contact={
